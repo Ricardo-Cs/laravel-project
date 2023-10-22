b83147604dd428eec63b07367d37f9b31a37bcf0
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-10-22 18:46:36 -0500

    Inserção de dados no banco por formulário

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Support extends Model
-{
+class Support extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'subject',
+        'body',
+        'status'
+    ];
 }
