8057923f52db078b2eb865b6af7f390470e0a80a
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-10-22 19:05:00 -0500

    Mostra detalhes de cada support

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
+// Get routes
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 
+// Post routes
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+
+// Put routes
+
+// Delete routes
