567c16ca896121fa096675b0f19f79028dd64093
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-10-27 18:28:43 -0500

    Header da estilização da página

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,29 +1,40 @@
-<h1>Listagem dos suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+@section('title', 'Fórum')
+    
+@section('header')
+@include('admin.supports.partials.header', compact('supports'))
+@endsection
 
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descrição</th>
-        <th>Ações</th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupportEnum($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Ir</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('content')
 
-<x-pagination 
-    :paginator="$supports"
-    :appends="$filters" />
+
+    <a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+    <table>
+        <thead>
+            <th>Assunto</th>
+            <th>Status</th>
+            <th>Descrição</th>
+            <th>Ações</th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupportEnum($support->status) }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">Ir</a>
+                        <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <x-pagination 
+        :paginator="$supports"
+        :appends="$filters" />
+
+@endsection
