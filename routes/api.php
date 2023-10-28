961e98990c4cc15c69302c6aa8879cdc86a82446
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-10-28 17:27:09 -0500

    Autorização da API com sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/supports', SupportApiController::class);
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/supports', SupportApiController::class);
+});
