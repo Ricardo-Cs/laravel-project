d6f705215f7d93e1ed70ab5e051d5ca4184173bc
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-11-08 17:54:45 -0500

    Resolve erro na query das respostas + ordena as respostas

--- app/Models/ReplySupport.php
+++ app/Models/ReplySupport.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -22,6 +23,11 @@ class ReplySupport extends Model {
 
     protected $with = ['user'];
 
+    protected static function booted(): void {
+        static::addGlobalScope('ancient', function (Builder $builder) {
+            $builder->latest();
+        });
+    }
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
