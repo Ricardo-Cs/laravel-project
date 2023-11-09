a221c65d36688824aae2a4e88c673b131935e537
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-11-09 17:41:23 -0500

    Redireciona para o login no caminho /

--- resources/views/admin/supports/partials/header.blade.php
+++ resources/views/admin/supports/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center gap-x-3">
             <h1 class="text-lg text-black-500">Fórum</h1>
 
-            <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvidas</span>
+            <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvida(s)</span>
         </div>
     </div>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('supports.index')" :active="request()->routeIs('supports')">
+                        {{ __('Supports') }}
+                    </x-nav-link>
                 </div>
             </div>
 
